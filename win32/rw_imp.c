3667437d1106ebac2bbecb8e37f64ac738e53e0a
Azarien <Azarien@users.noreply.github.com>
2016-11-04 23:05:15 +0100

    use PAGE_EXECUTE_READWRITE instead of PAGE_READWRITE

--- win32/rw_imp.c
+++ win32/rw_imp.c
@@ -387,7 +387,7 @@ void Sys_MakeCodeWriteable (unsigned long startaddr, unsigned long length)
 {
 	DWORD  flOldProtect;
 
-	if (!VirtualProtect((LPVOID)startaddr, length, PAGE_READWRITE, &flOldProtect))
+	if (!VirtualProtect((LPVOID)startaddr, length, PAGE_EXECUTE_READWRITE, &flOldProtect))
  		ri.Sys_Error(ERR_FATAL, "Protection change failed\n");
 }
 
